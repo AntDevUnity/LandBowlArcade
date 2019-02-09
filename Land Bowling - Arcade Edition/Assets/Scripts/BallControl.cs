851e395f976d4fd063f7489c040a95e4f3f0b1c8
ngomes007 <32779546+ngomes007@users.noreply.github.com>
2019-02-08 19:17:41 -0800

    everything

---
+++ Land Bowling - Arcade Edition/Assets/Scripts/BallControl.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallControl : MonoBehaviour {
+    public float speed = 10.0F; 
+     	private Rigidbody Rb;
+    public float rotationSpeed = 75.0f;
+
+    void Start()
+    {
+        Rb = GetComponent<Rigidbody>();
+        Cursor.lockState = CursorLockMode.Locked; 
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float translation = Input.GetAxis("Mouse Y") * speed;
+
+
+        float rotation = Input.GetAxis("Mouse X") * rotationSpeed;
+
+        rotation *= Time.deltaTime;
+
+        Vector3 movement = new Vector3(rotation, 0.0F, translation);
+        Rb.AddForce(movement * speed);
+
+        transform.Rotate(0, rotation, 0);
+        if (Input.GetKeyDown("escaps"))
+            Cursor.lockState = CursorLockMode.None;
+
+    }
+  
+    }
